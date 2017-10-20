39db619603b182ca71c55124d10ca272053ffeb8
Bill Guedel <wsguede@gmail.com>
2017-10-19 23:26:51 -0500

    Super basic app -- probably doesnt work

---
+++ src/main/java/com/trivia/TriviaAPI/TriviaApiApplication.java
@@ -0,0 +1,12 @@
+package com.trivia.TriviaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaApiApplication.class, args);
+	}
+}
